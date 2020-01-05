06106e0b75a350324161c7aaf39d10949ba87538
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2020-01-05 19:48:15 +0530

    9th commit

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -9,6 +9,19 @@ class NewTransaction extends StatelessWidget {
 
   final Function addTx;
 
+  void submitData() {
+    final enteredTitle = titleInput.text;
+    final enteredAmount = double.parse(amountInput.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   NewTransaction(this.addTx);
   @override
   Widget build(BuildContext context) {
@@ -22,6 +35,8 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: "Title"),
               controller: titleInput,
+              onSubmitted: (_) => submitData(),
+
               // onChanged:                       //here this, this will fired or excute on every key stroke
               // (val) {
               // titleInput = val;               // here we can write anything in the value of val and val always have the string which we have to pas as the arguments to the function here we have used the anonymous function
@@ -30,6 +45,13 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: "Amount"),
               controller: amountInput,
+              keyboardType: TextInputType
+                  .number, // this is kind of check now user only can enter the amount in the numbers because it will open the keyboard with the numbers only
+              onSubmitted: (_) =>
+                  submitData(), // here why we did this right it means when user enter all the values and press the check button on the keyboard then automatically trigger the submit button
+
+              // here it will take the function with the return type void but with the one arguments in the string so here we are using the anonymous function (_){} here in the anonymous function we pass _ this as a argument this means that yes we pass something flutter but we aren't in mood to using it
+
               // onChanged: (val2) {
               //   amountInput = val2;
               // },
@@ -37,16 +59,7 @@ class NewTransaction extends StatelessWidget {
             FlatButton(
               child: Text("Add Transaction"),
               textColor: Colors.purple,
-              onPressed: () {
-                addTx(
-                  titleInput.text,
-                  double.parse(amountInput.text), // here this will convert the amount in to the double
-                );
-                // print(titleInput.text);
-                // print(amountInput.text);
-                // print(titleInput);
-                // print(amountInput);
-              },
+              onPressed: submitData,
             ),
           ],
         ),
--- lib/widgets/transaction_lists.dart
+++ lib/widgets/transaction_lists.dart
@@ -3,65 +3,78 @@ import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
-   final List<Transaction> transactions;
+  final List<Transaction> transactions;
 
   TransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      height: 600,
-      child: SingleChildScrollView(    // here to use the single child scroll height should be fixed so where ever you want to use this single child scroll view must wrap widget into the container and set the height then use it as shown here bascially by this we can scroll down or up in case the number of lists increased 
+    return Container(
+      height: 500,
+      // child: SingleChildScrollView(    // here to use the single child scroll height should be fixed so where ever you want to use this single child scroll view must wrap widget into the container and set the height then use it as shown here bascially by this we can scroll down or up in case the number of lists increased
 
-              child: Column(
-                  children: transactions.map((tx) {
-                return Card(
-                    elevation: 5,
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          margin: EdgeInsets.symmetric(
-                            horizontal: 50,
-                            vertical: 20,
-                          ),
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          )),
-                          child: Text(
-                            '\$${tx.amount}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.purple,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              tx.title,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              DateFormat.yMMMd().format(tx.date),
-                              style: TextStyle(
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ));
-              }).toList() // here map() function coverts the list of objects into the list of widgets here at last we add tolist() this is because we know map will give us the iterable so in order to achieve the list we add this
-// here we should know that the map pick one by one element in the list
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+              elevation: 5,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 20,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    child: Text(
+                      '\$${transactions[index].amount.toStringAsFixed(2)}', // here this toStringAsFixed(2) will helps us not showing more than two numbers after the decimal point 
 
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.purple,
+                      ),
+                    ),
                   ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(transactions[index].date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ));
+        },
+        itemCount: transactions.length,
+        // here we are using the list view with the builder now here in the builder
+        // we have to use the itembuilder : now it will take the one function it must be an anonymous or some
+        // named Function now this function will have two arguments one is context here we named it ctx and
+        // the other must be index it can not be changed
+        //  here the height of the listview is the infinte so there must be a problem of not showing the widgets in the column wise sometimes due to its infinite height that's why you should always use the container while using the listview
+        // children: transactions.map((tx) {   // here in the list view we can not provide the children here we have to provide the itemcount which we want to be display or show on the screen
+
+        // here in the itembuilder the function which is anonymous in this case will be excuted automatically
+        // as much as the number of the itemcount is there but here it return the widget so here we want to
+        // return our card
+
+        // }).toList() // here map() function coverts the list of objects into the list of widgets here at last we add tolist() this is because we know map will give us the iterable so in order to achieve the list we add this
+// here we should know that the map pick one by one element in the list
       ),
+      // ),
     );
   }
 }
